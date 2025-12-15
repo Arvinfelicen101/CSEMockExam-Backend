05ab31e51437f5d40319107d9aa6d6d331c2760e
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-12-15 22:18:10 +0800

    Created test for register user

---
+++ CSEMockInterview.Tests/Services/UserManagement/UserManagementServicesTest.cs
@@ -0,0 +1,43 @@
+using CSEMockInterview.DTOs.Auth;
+using CSEMockInterview.Models;
+using CSEMockInterview.Repository.UserManagement;
+using CSEMockInterview.Services.UserManagement;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+
+
+namespace CSEMockInterview.Tests.Services.UserManagement;
+
+public class UserManagementServicesTest
+{
+
+    [Fact]
+    public async Task CreateUserService_PasswordMismatch_Returns404()
+    {
+        // Arrange
+        var repoMock = new Mock<IUserManagementRepository>();
+
+        var service = new UserManagementServices(repoMock.Object);
+
+        var user = new RegisterDTO()
+        {
+            username = "francis123",
+            firstName = "Francis",
+            middleName = "Pajarit",
+            lastName = "Flores",
+            password = "Francis123!",
+            confirmPassword = "Francis123?"
+        };
+
+        // Act
+        var result = await service.CreateUserService(user);
+
+        // Assert
+        Assert.False(result.success);
+        Assert.Equal(404, result.StatusCode);
+        Assert.Contains("password", result.Errors[0]);
+
+        repoMock.Verify(r => r.CreateUserAsync(It.IsAny<Users>(), It.IsAny<string>()), Times.Never);
+    }
+
+}
