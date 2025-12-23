d41efe734c69a198a64bbc63b7e12f83d022e32a
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-12-23 14:49:48 +0800

    add method

--- Backend/Repository/Importer/ImporterRepository.cs
+++ Backend/Repository/Importer/ImporterRepository.cs
@@ -1,4 +1,5 @@
 using Backend.Context;
+using Backend.Models;
 
 namespace Backend.Repository.Importer;
 
@@ -9,5 +10,13 @@ public class ImporterRepository : IImporterRepository
     public ImporterRepository(MyDbContext context)
     {
         _context = context;
+    }
+
+    public async Task SaveFKData(List<Category> categories, List<Paragraphs> paragraph, List<Choices> choices)
+    {
+        await _context.Category.AddRangeAsync(categories);
+        await _context.Paragraph.AddRangeAsync(paragraph);
+        await _context.Choice.AddRangeAsync(choices);
+        await _context.SaveChangesAsync();
     }
 }
