5832993296f01dd5a75ac1e573d94e0b4f62d414
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-12-25 21:05:21 +0800

    removed ientity

--- Backend/DTOs/Importer/ChoiceDTO.cs
+++ Backend/DTOs/Importer/ChoiceDTO.cs
@@ -1,9 +1,7 @@
 namespace Backend.DTOs.Importer;
 
-public class ChoiceDTO : IEntity
+public class ChoiceDTO
 {
-    public int Id { get; set; }
     public string ChoiceText { get; set; }
     public bool IsCorrect { get; set; }
-    public int QuestionId { get; set; }
 }
