5dddbaeae75e7b80eb4481feab796d803d2d3e05
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2026-01-04 21:36:21 +0800

    remove createchoice method

--- Backend/Services/ChoicesManagement/IChoiceService.cs
+++ Backend/Services/ChoicesManagement/IChoiceService.cs
@@ -4,9 +4,7 @@ namespace Backend.Services.ChoicesManagement
 {
     public interface IChoiceService
     {
-        Task CreateChoiceAsync(ChoiceCreateDTO choice);
         Task UpdateChoiceAsync(int id, ChoiceUpdateDTO choice);
-
         Task DeleteChoiceAsync(int id);
     }
 }
