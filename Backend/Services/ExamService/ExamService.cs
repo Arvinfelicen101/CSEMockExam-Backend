8952cb4b168737031e18914171774064a463e538
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2026-01-09 23:27:50 +0800

    create a exam service and a method for mapping exam data to entity

---
+++ Backend/Services/ExamService/ExamService.cs
@@ -0,0 +1,36 @@
+using Backend.Repository.ExamRepository;
+using Backend.DTOs.Exams;
+using Backend.Models;
+namespace Backend.Services.ExamService;
+
+public class ExamService : IExamService
+{
+    private readonly IExamRepository _repository;
+
+    public ExamService(IExamRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task SubmitExamService(List<UserExamAnswerDTO> exam)
+    {
+        var mapped = MapResults(exam);
+        await _repository.SubmitExamAsync(mapped);
+    }
+
+    private List<UserAnswers> MapResults(List<UserExamAnswerDTO> dtos)
+    {
+        var mappedData = new List<UserAnswers>();
+        foreach (var answer in dtos)
+        {
+            mappedData.Add(new UserAnswers()
+            {
+                UserId = answer.UserId,
+                QuestionId = answer.AnswerId,
+                Answer = answer.AnswerId
+            });
+        }
+
+        return mappedData;
+    }
+}
