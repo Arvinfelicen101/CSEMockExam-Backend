7937a88d0aa2b09e9937f9f5798102f08b0cc72a
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2026-01-13 20:59:28 +0800

    added more properties

--- Backend/DTOs/Question/QuestionListDTO.cs
+++ Backend/DTOs/Question/QuestionListDTO.cs
@@ -3,9 +3,15 @@
     public class QuestionListDTO
     {
         public required string QuestionName {  get; set; }
+        public required int categoryId { get; set; }
+        public required string categoryName { get; set; }
         public required int SubCategoryId { get; set; }
+        public required string SubCategoryName { get; set; }
         public int? ParagraphId { get; set; }
+        public string? ParagraphTxt { get; set; }
         public int YearPeriodId { get; set; }
+        public required int year { get; set; }
+        public required string period { get; set; }
   
     }
 }
