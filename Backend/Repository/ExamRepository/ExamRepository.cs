f8d9f54205ea7fc164145e8612335c38c5370bc7
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2026-01-30 12:24:24 +0800

    refactor fetching of questions

--- Backend/Repository/ExamRepository/ExamRepository.cs
+++ Backend/Repository/ExamRepository/ExamRepository.cs
@@ -1,5 +1,6 @@
 using Backend.Context;
 using Backend.Models;
+using Backend.DTOs.Exams;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Repository.ExamRepository;
@@ -13,15 +14,30 @@ public class ExamRepository : IExamRepository
         _context = context;
     }
     
-    public async Task<List<Questions>> GetAllAsync()
+    public async Task<List<CategoryDTO>> GetAllAsync()
     {
-        return await _context.Question
-            .Include(p => p.ParagraphNavigation)
-            .Include(s => s.SubCategoryNavigation!)
-            .ThenInclude(s => s.categoryNavigation)
-            .Include(y => y.YearPeriodNavigation)
-            .Include(c => c.ChoicesCollection)
-            .ToListAsync();
+        return await _context.Category
+            .AsNoTracking()
+            .AsSplitQuery()
+            .Select(c => new CategoryDTO(
+                c.Id,
+                c.CategoryName.ToString(),
+                c.SubCategoriesCollection.Select(sc => new SubCategoryDTO(
+                    sc.Id,
+                    sc.SubCategoryName,
+                    sc.QuestionsCollection.Select(q => new QuestionsDTO(
+                        q.Id,
+                        q.QuestionName,
+                        q.ChoicesCollection.Select(ch => new ChoicesDTO(
+                            ch.Id,
+                            ch.ChoiceText
+                            )).ToList()
+                        ))
+                        .ToList()
+                ))
+                    .ToList()
+            ))
+            .ToListAsync<CategoryDTO>();
     }
 
     public async Task SubmitExamAsync(List<UserAnswers> answer)
