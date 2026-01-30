a2b2877e2baa7183608ca750784a9b6faade94ee
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2026-01-30 11:53:31 +0800

    uncomment commented code

--- Backend/DTOs/Question/QuestionListDTO.cs
+++ Backend/DTOs/Question/QuestionListDTO.cs
@@ -4,8 +4,8 @@
     {
         public int QuestionId { get; set; }
         public required string QuestionName {  get; set; }
-        // public required int SubCategoryId { get; set; }
-        // public required string SubCategoryName { get; set; }
+        public required int SubCategoryId { get; set; }
+        public required string SubCategoryName { get; set; }
         public int? ParagraphId { get; set; }
         public string? ParagraphTxt { get; set; }
         public int YearPeriodId { get; set; }
