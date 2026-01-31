117d867f6568b8d09edda7886928d9e48bfe8341
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2026-01-31 12:10:35 +0800

    changed dto and refactor mapping and call check answer

--- Backend/Services/ExamService/ExamService.cs
+++ Backend/Services/ExamService/ExamService.cs
@@ -12,16 +12,18 @@ public class ExamService : IExamService
         _repository = repository;
     }
 
-    public async Task SubmitExamService(List<UserExamAnswerDTO> exam)
+    public async Task SubmitExamService(UserExamAnswerDTO exam)
     {
         var mapped = MapResults(exam);
         await _repository.SubmitExamAsync(mapped);
+        await _repository.CheckAnswersAsync(mapped);
+        await _repository.SaveChangesAsync();
     }
 
-    private List<UserAnswers> MapResults(List<UserExamAnswerDTO> dtos)
+    private List<UserAnswers> MapResults(UserExamAnswerDTO dtos)
     {
         var mappedData = new List<UserAnswers>();
-        foreach (var answer in dtos)
+        foreach (var answer in dtos.UserAnswer)
         {
             mappedData.Add(new UserAnswers()
             {
@@ -33,6 +35,7 @@ public class ExamService : IExamService
 
         return mappedData;
     }
+    
     
     public async Task<List<CategoryDTO>> GetAllAsync()
     {
